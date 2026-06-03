62f43ae52b37f8348e34a6b028bc7f04af3c0e62
arash-jj <arshjfry66@gmail.com>
2026-06-03 13:11:37 +0330

    add mongoDB connection

---
+++ lib/database/mongoose.ts
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+import { serverConfig } from "../config";
+import { AppError } from "../errors";
+
+export interface MongooseCache {
+  conn: mongoose.Connection | null;
+  promise: Promise<mongoose.Connection> | null;
+}
+
+declare global {
+  var mongooseCache: MongooseCache | undefined;
+}
+
+const cached: MongooseCache = global.mongooseCache || {
+  conn: null,
+  promise: null,
+};
+
+if (!global.mongooseCache) {
+  global.mongooseCache = cached;
+}
+
+const MONGODB_URI = serverConfig.DATABASE_URL!;
+
+if (!MONGODB_URI) throw  AppError.notFound("Please define MONGODB_URI environment variable");
+
+export async function connectDB(): Promise<mongoose.Connection> {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+      maxPoolSize: 10,
+      serverSelectionTimeoutMS: 5000,
+    };
+
+    cached.promise = mongoose
+      .connect(MONGODB_URI, opts)
+      .then((mongoose) => mongoose.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
