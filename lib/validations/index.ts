279a9aca734f7e386ac5b13f4ffde038141a0411
arash-jj <arshjfry66@gmail.com>
2026-06-06 17:56:28 +0330

    feat: implement jwt and update validations

--- lib/validations/index.ts
+++ lib/validations/index.ts
@@ -7,6 +7,10 @@ export const signInSchema = z.object({
 
 export const signUpSchema = z
   .object({
+    name: z
+      .string()
+      .min(2, "Name must be at least 2 characters")
+      .max(100, "Name is too long"),
     email: z.string().email("Invalid email address"),
     password: z
       .string()
