e129e47e44b7df8c6cc083a1d74b6f7f59bd20f4
arash-jj <arshjfry66@gmail.com>
2026-07-14 13:48:45 +0330

    feat: add webhook types

--- types/types.ts
+++ types/types.ts
@@ -1,4 +1,17 @@
 export interface NavItemsTypes {
   name: string;
   link: string;
+};
+
+export interface WebhookEvent {
+  id: string;
+  type: string; 
+  data: any;            
+  customerId: string | null;
+  provider: string;  
+}
+
+export interface PaymentProvider {
+  verifySignature(rawBody: string, signature: string): boolean;
+  parseEvent(rawBody: string): WebhookEvent;
 }
