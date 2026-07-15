7a1d4586e64e3a99f421d249b2df424523f8bdaa
arash-jj <arshjfry66@gmail.com>
2026-07-15 13:02:26 +0330

    feat: update types

--- types/types.ts
+++ types/types.ts
@@ -1,17 +1,26 @@
 export interface NavItemsTypes {
   name: string;
   link: string;
-};
+}
 
 export interface WebhookEvent {
   id: string;
-  type: string; 
-  data: any;            
+  type: string;
+  data: any;
   customerId: string | null;
-  provider: string;  
+  provider: string;
 }
 
 export interface PaymentProvider {
   verifySignature(rawBody: string, signature: string): boolean;
   parseEvent(rawBody: string): WebhookEvent;
+}
+
+export interface QueueJob {
+  id: string;
+  event: WebhookEvent;
+}
+
+export interface QueueAdapter {
+  enqueue(job: QueueJob, handler: (job: QueueJob) => Promise<void>): void;
 }
