f3302eea1ac63de74a83ae616e1ab4e5cbc06eec
arash-jj <arshjfry66@gmail.com>
2026-07-22 12:51:55 +0330

    fix: update update capacity of tokens in arcjet

--- lib/arcjet.ts
+++ lib/arcjet.ts
@@ -1,6 +1,4 @@
 import arcjet, { tokenBucket } from "@arcjet/next";
-import { NextRequest } from "next/server";
-import { QuotaService } from "@/services/quota-tenant.service";
 import { serverConfig } from "./config";
 
 export const aj = arcjet({
@@ -10,19 +8,9 @@ export const aj = arcjet({
     tokenBucket({
       mode: "LIVE",
       refillRate: 10,
-      interval: 60, // seconds
-      // Dynamic limit based on tenant
-      async max(req: NextRequest) {
-        const tenantId = req.headers.get("x-tenant-id");
-        if (!tenantId) return 100; // fallback
-
-        try {
-          const service = new QuotaService(tenantId);
-          return await service.getApiRateLimit();
-        } catch {
-          return 100; // error fallback
-        }
-      },
-    } as any),
+      interval: 60,
+      // for dynamic capacity (for tenants) -> change the token bucket method
+      capacity: 100
+    }),
   ],
 });
