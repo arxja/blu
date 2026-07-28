bc1b1474529e0423da40dfd4f669f1d515bda495
arash-jj <arshjfry66@gmail.com>
2026-07-28 12:07:17 +0330

    feat: update constants file structure

---
+++ lib/constants/navigation.constants.ts
@@ -0,0 +1,24 @@
+import { NavItemsTypes } from "@/types/types";
+
+export const NAVBAR_ITEMS: NavItemsTypes[] = [
+  {
+    name: "Pricing",
+    link: "/pricing",
+  },
+  {
+    name: "About",
+    link: "/#",
+  },
+  {
+    name: "Contact",
+    link: "/#",
+  },
+];
+
+export const FOOTER_ITEMS: NavItemsTypes[] = [
+  // Add footer navigation items
+];
+
+export const MOBILE_NAV_ITEMS: NavItemsTypes[] = [
+  // Add mobile-specific navigation items
+];
